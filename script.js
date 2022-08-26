81287b5dd3a33ba4ff0be743d731358969b08078
Salvador Mejia <salvador.josemc@gmail.com>
2022-08-25 19:33:14 -0600

    Add scores and finish game.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 //Create array of choices
 const choices = ['rock', 'paper', 'scissors'];
 
+//Create score log
+let playerScore = 0;
+let computerScore = 0;
+
 //Create a random choice
 function getComputerChoice() {
     let randomize = Math.floor(Math.random() * choices.length);
@@ -11,7 +15,7 @@ function getComputerChoice() {
     } else if (randomize === 1) {
         return choices[1];
     } else if (randomize === 2) {
-       return choices[2];
+        return choices[2];
     }
 }
 
@@ -20,33 +24,46 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === choices[0]) && (computerSelection === choices[0])) {
         return "It's a tie! Rock equals Rock";
     } else if ((playerSelection === choices[0]) && (computerSelection === choices[1])) {
+        computerScore++;
         return "You lose! Paper beats Rock";
     } else if ((playerSelection === choices[0]) && (computerSelection === choices[2])) {
+        playerScore++;
         return "You win! Rock beats Scissors";
     } else if ((playerSelection === choices[1]) && (computerSelection === choices[0])) {
+        playerScore++;
         return "You win! Paper beats rock";
     } else if ((playerSelection === choices[1]) && (computerSelection === choices[1])) {
         return "It's a tie! Paper equals Paper";
     } else if ((playerSelection === choices[1]) && (computerSelection === choices[2])) {
+        computerScore++;
         return "You lose! Scissors beats Paper";
     } else if ((playerSelection === choices[2]) && (computerSelection === choices[0])) {
+        computerScore++;
         return "You lose! Rock beats Scissors";
     } else if ((playerSelection === choices[2]) && (computerSelection === choices[1])) {
+        playerScore++;
         return "You win! Scissors beats Paper";
     } else if ((playerSelection === choices[2]) && (computerSelection === choices[2])) {
         return "It's a tie! Scissors equals Scissors";
-    } 
+    }
 }
 
-//Test results
-const playerSelection = prompt();
-const computerSelection = getComputerChoice();
+/*Test results
+let playerSelection = prompt('rock, paper, scissors!');
+let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+*/
 
 //Create function to play several rounds
 function game() {
-   let games = playRound();
     for (let i = 0; i < 5; i++) {
-       if 
+        let playerSelection = prompt('rock, paper, scissors!');
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your score is: " + playerScore);
+        console.log("Computer score is: " + computerScore);
     }
-}
+    alert("Game Over");
+};
+
+game();
